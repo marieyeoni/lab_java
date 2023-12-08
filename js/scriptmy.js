682b8464ec7cf864da15f309d2de69db36fb99cb
최서연 <20220855@dongduk.ac.kr>
2023-12-08 21:04:18 +0900

    change

--- js/scriptmy.js
+++ js/scriptmy.js
@@ -1,7 +1,7 @@
 function enter() {
     var binarynum = document.getElementById("binarynum").value;
-    var hex = parseInt(binarynum, 2).toString(16).toUpperCase();
-    document.getElementById("hexa").value = hex;
+    var hexa = parseInt(binarynum, 2).toString(16).toUpperCase();
+    document.getElementById("hexa").value = hexa;
 }
 
 function pascalValue(row, col) {
